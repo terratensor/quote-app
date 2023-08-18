d1eeefd8f9ccb976bdd2e662a109ee3e3edc2c3a
agusev <audetv@gmail.com>
2023-08-18 17:48:18 +0300

    Changed main, need handlers

--- api/quotesrv/cmd/quotesrv/main.go
+++ api/quotesrv/cmd/quotesrv/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"gitgub.com/terratensor/quote-app/api/quotesrv/internal/api/server"
+	"gitgub.com/terratensor/quote-app/api/quotesrv/internal/app/repos/quote"
+	"gitgub.com/terratensor/quote-app/api/quotesrv/internal/app/starter"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -21,5 +27,23 @@ func main() {
 	log.Printf("Local time zone %s. Service started at %s", tz,
 		tnow.Format("2006-01-02T15:04:05.000 MST"))
 
-	//ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	var qst quote.Store
+	a := starter.NewApp(qst)
+	qs := quote.NewQuotes(qst)
+	// ToDo сделать handler и routeroapi
+	h := handler.NewHandlers(qs)
+	rh := routeroapi.NewRouterOpenAPI(h)
+
+	srv := server.NewServer(os.Getenv("SERVER_ADDR"), rh)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go a.Serve(ctx, wg, srv)
+
+	<-ctx.Done()
+	cancel()
+	wg.Wait()
 }
